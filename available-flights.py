824c5d678a5903e65b68ed9c72c0f128a1196ba9
Alenuhuh <alexandraneamti6@gmail.com>
2025-01-11 15:04:58 +0200

    solitare

--- available-flights.py
+++ available-flights.py
@@ -8,8 +8,7 @@ flights ={
     'Seol':[{'date': datetime(2025, 2, 2) , 'pret' : 500}],
     'New York':[{'date': datetime(2025, 1, 30, 8) , 'pret' : 450}],
     'Las Vegas':[{'date': datetime(2025, 2, 15, 3) , 'pret' : 600}],
-    'Singapore':[{'date': }]
-    'Tokyo':[{'date': }]
-
+    'Singapore':[{'date': datetime(2025, 6, 7, 12) , 'pret' : 300}],
+    'Tokyo':[{'date': datetime(2025, 3, 16, 18) , 'pret' : 550}]
 }
 
